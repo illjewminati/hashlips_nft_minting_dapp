41ceff609e2f7e2b840a7bc081dda6e9a4bdd60a
illjewminati <103166826+illjewminati@users.noreply.github.com>
2022-06-11 13:43:06 -0500

    app js edit \n

--- src/App.js
+++ src/App.js
@@ -99,8 +99,7 @@ function App() {
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
-  const [feedback, setFeedback] = useState(`Click "Touch Grass" to mint your smol plot of peace.\N
-  PROCEEDS TO FUND A UNIQUE 1/1 GRASS TOUCHING EXPERIECE WITH A WEB3 BILLIONAIRE!`);
+  const [feedback, setFeedback] = useState(`Click "Touch Grass" to mint your smol plot of peace. \nPROCEEDS TO FUND A UNIQUE 1/1 GRASS TOUCHING EXPERIECE WITH A WEB3 BILLIONAIRE!`);
   const [mintAmount, setMintAmount] = useState(1);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
